e96a2167f73c9566d1e9166c13b2fe4601ccb119
Keven Costa <ogkeven278@gmail.com>
2025-12-05 13:58:15 -0300

    add entidade cliente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cliente } from './cliente/entities/cliente.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port:  3306,
+      username: 'root',
+      password: 'root', // Mudar a senha do banco de dados
+      database: 'db_crm',
+      entities: [Cliente],
+      synchronize: true,
+      logging: true
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
